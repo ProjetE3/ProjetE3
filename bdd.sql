0c4604a6c934302d1431d9a12bc0ec343fa1e1f3
toch79 <theo.heurtebize@outlook.fr>
2018-05-23 08:50:44 +0200

    bdd v1.2-Modification syntaxe

--- bdd.sql
+++ bdd.sql
@@ -5,8 +5,8 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Base de données: `HestiaDB`
 --
-CREATE DATABASE 'HestiaDB';
-USE 'HestiaDB';
+CREATE DATABASE HestiaDB;
+USE HestiaDB;
 
 
 -- Drop Table Section-----------------------------------------
