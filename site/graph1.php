7d9563e2cf3586642e4d6557e3be21b8941df0da
toch79 <theo.heurtebize@outlook.fr>
2018-06-04 10:08:52 +0200

    site modifié par Emilie 01/06

---
+++ site/graph1.php
@@ -0,0 +1,49 @@
+<?php // content="text/plain; charset=utf-8"
+require_once ('jpgraph/src/jpgraph.php');
+require_once ('jpgraph/src/jpgraph_line.php');
+
+
+//donnée sur la ligne
+$datay1 = array(20,15,23,15);
+
+
+// Setup the graph
+// Taille du graphe
+$graph = new Graph(300,250);
+$graph->SetScale("textlin");
+
+$theme_class=new UniversalTheme;
+
+$graph->SetTheme($theme_class);
+$graph->img->SetAntiAliasing(false);
+
+//titre du graphe
+$graph->title->Set('Coucou');
+$graph->SetBox(false);
+
+$graph->SetMargin(40,20,36,63);
+
+$graph->img->SetAntiAliasing();
+
+$graph->yaxis->HideZeroLabel();
+$graph->yaxis->HideLine(false);
+$graph->yaxis->HideTicks(false,false);
+
+$graph->xgrid->Show();
+$graph->xgrid->SetLineStyle("solid");
+$graph->xaxis->SetTickLabels(array('A','B','C','D'));
+$graph->xgrid->SetColor('#E3E3E3');
+
+// Create the first line
+$p1 = new LinePlot($datay1);
+$graph->Add($p1);
+$p1->SetColor("#6495ED");
+$p1->SetLegend('Line 1');
+
+
+$graph->legend->SetFrameWeight(1);
+
+// Output line
+$graph->Stroke();
+
+?>
